d6a6c5445a59e9b1d0c82e9f0e4b7615574d083c
nehakumari02 <nehapanwal02@gmail.com>
2025-02-06 09:28:01 +0530

    login and signup with password bug 3

--- index.js
+++ index.js
@@ -13,11 +13,25 @@ require('./models/dbConnection')
 
 const cors = require('cors')
 
-app.use(cors({
-    origin: process.env.ORIGIN,
-    credentials: true
-}));
-app.options('*', cors()); 
+// CORS Configuration
+const corsOptions = {
+    origin: process.env.ORIGIN,  // Ensure this matches your frontend URL
+    credentials: true,           // Allow cookies to be sent
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
+};
+
+app.use(cors(corsOptions));
+
+app.options('*', cors(corsOptions)); 
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', process.env.ORIGIN);
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+});
 
 app.get('/',(req,res)=>{
     res.send("Hello from auth server")
